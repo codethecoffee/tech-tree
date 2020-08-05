898c9a199de08c9930928a9b61e3995f244d1b63
codethecoffee <sujunglee2020@u.northwestern.edu>
2020-08-05 16:25:07 +0900

    Initial Commit

---
+++ Tech Tree/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Tech Tree
+//
+//  Created by Suzy Lee on 8/5/20.
+//  Copyright © 2020 Suzy Lee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
